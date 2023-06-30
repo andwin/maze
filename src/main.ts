7fd1cedf3cf1cb7cc3a00a3c64b46e941b9e0f30
Anders Windelhed <anders.windelhed@tv4.se>
2023-06-30 23:04:19 +0200

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { Application } from 'pixi.js'
+
+import './style.css'
+
+const app = new Application({
+  width: 800,
+  height: 600,
+  backgroundColor: 0x1099bb,
+ })
+
+ document.querySelector('#app')!.appendChild(app.view as HTMLCanvasElement)
