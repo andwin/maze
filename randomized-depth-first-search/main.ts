abfec4670abeec86c94f7fed002bdbd207145bc2
Anders Windelhed <anders.windelhed@tv4.se>
2023-07-03 22:59:21 +0200

    Implementing recursive randomized depth-first search

--- randomized-depth-first-search/main.ts
+++ randomized-depth-first-search/main.ts
@@ -45,8 +45,68 @@ const app = new PIXI.Application({
 
 document.querySelector('#app')!.appendChild(app.view as HTMLCanvasElement)
 
-const run = () => {
-  draw()
+/**
+ * Randomized depth-first search - recursive implementation
+ *
+ * https://en.wikipedia.org/wiki/Maze_generation_algorithm
+ *
+ * 1. Given a current cell as a parameter
+ * 2. Mark the current cell as visited
+ * 3. While the current cell has any unvisited neighbour cells
+ *   1. Choose one of the unvisited neighbours
+ *   2. Remove the wall between the current cell and the chosen cell
+*    3. Invoke the routine recursively for the chosen cell
+*/
+
+const run = (currentCell: Cell) => {
+  currentCell.visited = true
+
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    const neighbours = getNeighbours(currentCell)
+    if (!neighbours.length) return
+
+    const nextCell = randomItem(neighbours)
+    removeWall(currentCell, nextCell)
+
+    run(nextCell)
+  }
+}
+
+const getNeighbours = (cell: Cell): Cell[] => {
+  const neighbours: Cell[] = []
+  const { row, col } = cell
+
+  if (cells[row - 1]?.[col]?.visited === false) neighbours.push(cells[row - 1][col])
+  if (cells[row]?.[col + 1]?.visited === false) neighbours.push(cells[row][col + 1])
+  if (cells[row + 1]?.[col]?.visited === false) neighbours.push(cells[row + 1][col])
+  if (cells[row]?.[col - 1]?.visited === false) neighbours.push(cells[row][col - 1])
+
+  return neighbours
+}
+
+const randomItem = <T>(items: T[]): T => items[Math.floor(Math.random() * items.length)]
+
+const removeWall = (currentCell: Cell, nextCell: Cell) => {
+  if (currentCell.row > nextCell.row) {
+    currentCell.top = false
+    nextCell.bottom = false
+  }
+
+  if (currentCell.col < nextCell.col) {
+    currentCell.right = false
+    nextCell.left = false
+  }
+
+  if (currentCell.row < nextCell.row) {
+    currentCell.bottom = false
+    nextCell.top = false
+  }
+
+  if (currentCell.col > nextCell.col) {
+    currentCell.left = false
+    nextCell.right = false
+  }
 }
 
 const draw = () => {
@@ -84,4 +144,6 @@ const draw = () => {
   app.stage.addChild(graphics)
 }
 
-run()
+const initalCell = cells[0][0]
+run(initalCell)
+draw()
